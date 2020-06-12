cf027360f04f670e58bc5a5566b7b91d9123ae75
Mohammed Gulam Rabbi <41266775+rabbi-630@users.noreply.github.com>
2020-06-13 00:40:00 +0600

    Create copy.js

---
+++ copy.js
@@ -0,0 +1,9 @@
+<script type='text/javascript'>
+      //<![CDATA[
+$(document).ready(function () {
+  $('#copyright-template').html('Template designed by <a href='https://www.Thugsofbd.com' >ThugsofBD</a>');
+  setInterval(function () {
+      if (!$('#copyright-template:visible').length) window.location.href = 'https://www.thugsofbd.com'
+  }, 2000)})
+      //]]>
+</script>
