e8700ed2b8fcdb29b7c633938e9f8154e6957428
Mohammed Gulam Rabbi <41266775+rabbi-630@users.noreply.github.com>
2020-06-22 02:24:29 +0600

    Delete tbd.js

--- tbd.js
+++
@@ -1,9 +0,0 @@
-<script type = 'text / javascript'>
-      // <! [CDATA [
-$ (document). ready (function () {
-  $ ('# copyright-template'). html ('Template designed by <a href =' https : //www.bacsiwindows.com '> Windows Doctor </a>');
-  setInterval (function () {
-      if (! $ ('# copyright-template: visible'). length) window.location.href = ' https: // www bacsiwindows.com '
-  }, 2000 )})
-      //]]>
-</script>
